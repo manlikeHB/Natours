6d778584144fcf579b3af6d620c203ac59e60953
HB <hbyusuf40@gmail.com>
2022-10-13 12:30:28 +0100

    Create error controller

---
+++ controllers/errorController.js
@@ -0,0 +1,54 @@
+const AppError = require('./../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return next(new AppError(message, 400));
+};
+
+// const handleDuplicateFieldsDB = () => {};
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  console.log(err);
+
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.error('ERROR', err);
+
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong',
+    });
+  }
+};
+
+module.exports = (err, req, res, next) => {
+  // console.log(err.stack);
+
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    let error = { ...err };
+
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    // if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    // if (error.name === 'ValidationError')
+
+    sendErrorProd(error, res);
+  }
+};
