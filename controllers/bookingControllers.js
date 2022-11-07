7d7a313088d14b8374e2a57bd25d8ce4dff5ce32
HB <hbyusuf40@gmail.com>
2022-11-07 10:30:43 +0100

    Bugfix

--- controllers/bookingControllers.js
+++ controllers/bookingControllers.js
@@ -61,7 +61,7 @@ const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_details.email }))
     .id;
-  const price = session.data.object.amount_total / 100;
+  const price = session.amount_total / 100;
 
   await Booking.create({ tour, user, price });
 };
